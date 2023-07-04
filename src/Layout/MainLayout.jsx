fe56f1e2d9ad37a1a1b8f41c66385f0f6acccee8
Redwan islam <redwanislam2760@gmail.com>
2023-07-04 23:44:17 +0600

    added another navbar

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../Pages/Shared/Navbar/Navbar";
+// import Navbar from "../Pages/Shared/Navbar/Navbar";
 import Footer from "../Pages/Shared/Footer/Footer";
-
+import Navbar from "../Pages/Shared/Navbar";
 const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
+      {/* <Navbar /> */}
+      <Navbar />
       <Navbar />
       <div className="container lg:w-[80%] mx-auto">
         <Outlet />
